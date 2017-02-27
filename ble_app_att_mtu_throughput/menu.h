9dbb757eab1fc5e25470d49a9d25df300d2bc649
Bauck, Ole <Ole.Bauck@nordicsemi.no>
2017-02-27 16:22:28 +0100

    added license information

--- ble_app_att_mtu_throughput/menu.h
+++ ble_app_att_mtu_throughput/menu.h
@@ -1,3 +1,14 @@
+/* Copyright (c) 2017 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */ 
 
 #ifndef MENU_H
 #define MENU_H
